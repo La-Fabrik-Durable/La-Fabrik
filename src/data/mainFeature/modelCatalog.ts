6227caa8192aaee66b3220b084d81f3971faaa52
Tom Boullay <tom@famille-boullay.fr>
2026-04-29 23:30:40 +0200

    feat: expand main feature model catalog

--- src/data/mainFeature/modelCatalog.ts
+++ src/data/mainFeature/modelCatalog.ts
@@ -4,10 +4,23 @@ export interface ModelCatalogItem {
 }
 
 export const MAIN_FEATURE_MODEL_CATALOG: ModelCatalogItem[] = [
+  { name: "Electricienne", path: "/models/elecsimple/model.gltf" },
+  { name: "Electricienne complete", path: "/models/elec/model.gltf" },
+  { name: "Eolienne", path: "/models/eolienne/model.gltf" },
+  { name: "Fermier", path: "/models/fermier/model.gltf" },
+  { name: "Galet", path: "/models/galet/model.gltf" },
+  { name: "Gant", path: "/models/gant/model.gltf" },
+  { name: "Gants", path: "/models/gants/model.gltf" },
+  { name: "Gerant", path: "/models/gerant/model.gltf" },
+  { name: "Immeuble", path: "/models/immeuble1/model.gltf" },
   { name: "Kit de relance", path: "/models/packderelance/model.gltf" },
-  { name: "Talkie", path: "/models/talkie/model.gltf" },
+  { name: "La Fabrik", path: "/models/lafabrik/model.gltf" },
+  { name: "Maison", path: "/models/maison1/model.gltf" },
+  { name: "Map", path: "/models/map/model.gltf" },
+  { name: "Perso principal", path: "/models/persoprincipal/model.gltf" },
+  { name: "Pylone", path: "/models/pylone/model.gltf" },
   { name: "Refroidisseur", path: "/models/refroidisseur/model.gltf" },
   { name: "Sapin", path: "/models/sapin/model.gltf" },
-  { name: "Gant", path: "/models/gant/model.gltf" },
-  { name: "Galet", path: "/models/galet/model.gltf" },
+  { name: "Talkie", path: "/models/talkie/model.gltf" },
+  { name: "Terrain", path: "/models/terrain/model.gltf" },
 ];
