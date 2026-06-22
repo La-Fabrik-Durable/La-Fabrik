0f560e7f91ff8e5df74f0c6ae6bf4dfc8f24d313
Tom Boullay <tom@famille-boullay.fr>
2026-06-22 13:39:32 +0200

    feat: inject NersansOne font dynamically from R2 via FontLoader

---
+++ src/components/ui/FontLoader.tsx
@@ -0,0 +1,24 @@
+import { useEffect } from 'react'
+import { assetUrl } from '@/utils/assetUrl'
+
+export function FontLoader() {
+  useEffect(() => {
+    const style = document.createElement('style')
+    style.textContent = `
+      @font-face {
+        font-family: 'NersansOne';
+        src: url('${assetUrl('/fonts/NersansOne.woff2')}') format('woff2'),
+             url('${assetUrl('/fonts/NersansOne.woff')}') format('woff');
+        font-weight: normal;
+        font-style: normal;
+        font-display: swap;
+      }
+    `
+    document.head.appendChild(style)
+    return () => {
+      document.head.removeChild(style)
+    }
+  }, [])
+
+  return null
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { FontLoader } from "@/components/ui/FontLoader";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <FontLoader />
     <App />
   </StrictMode>,
 );
